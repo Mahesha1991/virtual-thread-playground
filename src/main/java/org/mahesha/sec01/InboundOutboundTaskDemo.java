12d9979eecb9db22f04820f976f075be9f20070e
mahesha1991 <mahesharaoy@gmail.com>
2025-03-31 18:41:31 -0700

    Virtual Threads

--- src/main/java/org/mahesha/sec01/InboundOutboundTaskDemo.java
+++ src/main/java/org/mahesha/sec01/InboundOutboundTaskDemo.java
@@ -1,5 +1,7 @@
 package org.mahesha.sec01;
 
+import java.util.concurrent.CountDownLatch;
+
 public class InboundOutboundTaskDemo {
 
     //private static final int MAX_PLATFORM = 1_000_000;
@@ -7,9 +9,13 @@ public class InboundOutboundTaskDemo {
     // -Xss option will help to adjust the stack size
     private static final int MAX_PLATFORM = 10;
 
-    public static void main(String[] args) {
+    // For Virtual threads, we do not see out of memory threads. This is because it is like an illusion provided by java
+    private static final int MAX_VIRTUAL = 10;
 
-        platformThreadDemo();
+    public static void main(String[] args) throws InterruptedException {
+
+        vitualThreadDemo();
+        //platformThreadDemo2();
 
     }
 
@@ -21,6 +27,56 @@ public class InboundOutboundTaskDemo {
             Thread thread = new Thread(() -> Task.ioIntensive(finalI));
             thread.start();
         }
+    }
 
+    private static void platformThreadDemo2(){
+
+        Thread.Builder.OfPlatform builder = Thread.ofPlatform().name("mahesha", 1);
+        for(int i = 0; i < MAX_PLATFORM; i++){
+
+            int finalI = i;
+            Thread thread = builder.unstarted(() -> Task.ioIntensive(finalI));
+            thread.start();
+        }
+    }
+
+    private static void platformThreadDemo3() throws InterruptedException {
+
+        //Deamon threads will be spawn and our main thread will exit if we do not add the latch
+        CountDownLatch latch = new CountDownLatch(MAX_PLATFORM);
+
+        Thread.Builder.OfPlatform builder = Thread.ofPlatform().daemon().name("daemon", 1);
+        for(int i = 0; i < MAX_PLATFORM; i++){
+
+            int finalI = i;
+            Thread thread = builder.unstarted(() -> {
+                Task.ioIntensive(finalI);
+                latch.countDown();
+            });
+            thread.start();
+        }
+        latch.await();
+    }
+
+
+
+    // Create Virtual thread using Builder. Virtual Threads are Daemon Threads By Default.
+    //Virtual Threads will not have any names by default
+    private static void vitualThreadDemo() throws InterruptedException {
+
+        CountDownLatch latch = new CountDownLatch(MAX_PLATFORM);
+
+        Thread.Builder.OfVirtual builder = Thread.ofVirtual().name("virtual-", 1);
+        for(int i = 0; i < MAX_VIRTUAL; i++){
+
+            int finalI = i;
+            Thread thread = builder.unstarted(() -> {
+                Task.ioIntensive(finalI);
+                latch.countDown();
+            });
+            thread.start();
+        }
+
+        latch.await();
     }
 }
--- src/main/java/org/mahesha/sec01/Task.java
+++ src/main/java/org/mahesha/sec01/Task.java
@@ -13,9 +13,9 @@ public class Task {
     public static void ioIntensive(int i){
 
         try{
-            log.info("Strarting I/O task{} ", i);
+            log.info("Strarting I/O task{} Thread Info: {} ", i, Thread.currentThread());
             Thread.sleep(Duration.ofSeconds(10));
-            log.info("Ending I/O task{} ", i);
+            log.info("Ending I/O task{} Thread Info: {} ", i, Thread.currentThread());
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
