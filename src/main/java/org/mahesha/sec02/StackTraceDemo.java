10c131cfcb9b25fda01991eef8352bc409b0bf81
mahesha1991 <mahesharaoy@gmail.com>
2025-04-01 20:30:29 -0700

    virtual threads

---
+++ src/main/java/org/mahesha/sec02/StackTraceDemo.java
@@ -0,0 +1,27 @@
+package org.mahesha.sec02;
+
+import org.mahesha.util.CommonUtils;
+
+import java.time.Duration;
+
+public class StackTraceDemo {
+
+    public static void main(String[] args) {
+
+        //demo(Thread.ofPlatform());
+
+
+        demo(Thread.ofVirtual().name("virtual-", 1));
+        // We need the below as Virtual threads are daemon threads
+        CommonUtils.sleep(Duration.ofSeconds(2));
+
+    }
+
+    private static void demo(Thread.Builder builder){
+
+        for(int i = 1; i <=20; i++){
+            int j = i;
+            builder.start(() -> Task.execute(j));
+        }
+    }
+}
